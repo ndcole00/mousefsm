f8bbf53c0929886299c825a8bfcc37eab1ba13c9
adilgkhan <khan.adil@kcl.ac.uk>
2019-01-30 20:28:37 +0000

    Fixed bug with right side screen acting strange, flashing etc, specially when changing stimPosition

--- mapping_stim/KCL_retinotopy_flicker.m
+++ mapping_stim/KCL_retinotopy_flicker.m
@@ -1,3 +1,4 @@
+
 function rs_2eyes_retinotopy_flicker
 
 % (15*2*10*1.3)./60
@@ -94,7 +95,7 @@ try,
     screens=Screen('Screens');
 
     % Open double-buffered onscreen window with the requested stereo mode:
-    screenNumber=2;
+    screenNumber=3;
     stereoMode=0;
     [expWin,screenRect]=Screen('OpenWindow',screenNumber,STM.rgbgray,[],[],[],stereoMode);
     
