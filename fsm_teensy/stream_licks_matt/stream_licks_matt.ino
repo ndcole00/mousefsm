0e76ce5fcbf00298b823c8ee40154a789f5df3f8
BZ-WG6-PLW10\Adil <adilgkhan@gmail.com>
2021-07-19 18:07:05 +0100

    To allow gating the signal which is pulsing at 40hz with a digital line. It should stop pulsing as long as the digital gating line is high

--- fsm_teensy/stream_licks_matt/stream_licks_matt.ino
+++ fsm_teensy/stream_licks_matt/stream_licks_matt.ino
@@ -12,7 +12,8 @@ int threshA = .7*1000;//
 int threshB = .7*1000 + 1000;//
 
 int analogoutPin = 23;
-
+int gatingPin = 0;// Whenever this pin goes high, it should stop the output on analogoutPin
+int optoPulseFreq = 40;
 
 unsigned long PrevMillis = millis();
 unsigned long CurrMillis = millis();
@@ -20,11 +21,14 @@ unsigned long CurrMillis = millis();
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
+  pinMode(gatingPin, INPUT);
   pinMode(analogoutPin, OUTPUT);
   analogWriteResolution(8);
-  analogWriteFrequency(analogoutPin, 40);
+  analogWriteFrequency(analogoutPin, optoPulseFreq);
   analogWrite(analogoutPin,128);
-
+  //The value is between 0 to 256, where the higher the value, the more time the signal remains high. 
+  //0 and 256 correspond to always low and always high. A value of 128 is 50% duty cycle
+  int optoRunning = 1;
 }
 
 
@@ -51,6 +55,18 @@ void loop()
     digitalWrite(ledPin, LOW);
     delay(10);
     digitalWrite(ledPin, HIGH);
+  }
+
+  // Check if gatingPin is high to stop the opto output
+  if (digitalRead(gatingPin)&& optoRunning==1) {// just run this once when gatingPin goes high
+    // stop the optopulsing
+    analogWrite(analogoutPin,0);
+    optoRunning = 0;
+  }
+  if  (digitalRead(gatingPin)==0 && optoRunning==0) {// just run this once when gatingPin goes low
+    // restart the optopulsing
+    analogWrite(analogoutPin,128);
+    optoRunning = 1;
   }
 
 }
